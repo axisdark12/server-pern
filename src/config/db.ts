dc5bf71a7a54716d2f77b026e5997257000dfab5
Alexis <usuario1193@MacBook-Pro-de-Alexis.local>
2025-07-25 14:09:25 -0600

    con pruebas activas

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv'
+
 dotenv.config()
-const db = new Sequelize(process.env.DB_URL, {
+const db = new Sequelize(process.env.DB_URL!, {
     models: [__dirname + '/../models/**/*.ts']
 })
 
